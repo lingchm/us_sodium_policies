217c3458d9c0c0895efeac2302865b1e9c582f63
lingchm <lingchm@gmail.com>
2022-07-14 19:27:50 -0400

    graph analysis

--- code/full_archive_search.py
+++ code/full_archive_search.py
@@ -3,17 +3,19 @@ import os
 import pandas as pd
 from time import sleep
 import copy
+from datetime import datetime
 
 '''
-Query params: https://developer.twitter.com/en/docs/twitter-api/tweets/search/api-reference/get-tweets-search-recent
-
+Query params: 
+    https://developer.twitter.com/en/docs/twitter-api/tweets/search/api-reference/get-tweets-search-recent
+https://developer.twitter.com/en/docs/twitter-api/tweets/lookup/api-reference/get-tweets#tab1
 '''
 
 
 # To set your environment variables in your terminal run the following line:
 # export 'BEARER_TOKEN'='<your_bearer_token>'
-# BEARER_TOKEN = "AAAAAAAAAAAAAAAAAAAAAOzpeAEAAAAAzb%2FX4CQSTJ6wGFZLI3UK%2F1DHUa8%3D8UhFs7G9kuwBr9nycPKyph3xDwDgx0apMdNsRhpRYmEZN1yQeh"
-BEARER_TOKEN = os.environ.get("BEARER_TOKEN")
+BEARER_TOKEN = "AAAAAAAAAAAAAAAAAAAAAOzpeAEAAAAAzb%2FX4CQSTJ6wGFZLI3UK%2F1DHUa8%3D8UhFs7G9kuwBr9nycPKyph3xDwDgx0apMdNsRhpRYmEZN1yQeh"
+# BEARER_TOKEN = os.environ.get("BEARER_TOKEN")
 SEARCH_URL = "https://api.twitter.com/2/tweets/search/all"
    
 # Optional params: start_time,end_time,since_id,until_id,max_results,next_token,
@@ -79,33 +81,21 @@ def jason_to_df(json_response):
     return tweets
     
 
-def get_tweets_by_user(username, export_file):
+def get_tweets_by_user(username, query_params, export_file):
     """
     Pull tweets and and put them in a text file.
     """
-    
-    query_params = {'query': 'from:'+username,
-                'start_time': '2006-07-01T00:00:00Z',
-                'end_time': '2022-06-01T00:00:00Z',
-                'max_results': "100",
-                'sort_order':'recency',
-                'expansions':'author_id,geo.place_id', #'in_reply_to_user_id,entities.mentions.username,referenced_tweets.id.author_id
-                'place.fields':'contained_within,country,country_code,full_name,geo,id,name,place_type',
-                'tweet.fields':'author_id,created_at,entities,geo,id,in_reply_to_user_id,lang,public_metrics,referenced_tweets,text',
-                'user.fields':'created_at,description,id,name,username',
-                'next_token': None
-                }
-
     json_response = connect_to_endpoint(SEARCH_URL, query_params)
     tweets = jason_to_df(json_response)
     tweets_all = tweets.copy()
-    
     counter = 1
     tweets_counter = json_response["meta"]["result_count"]
     print(username, counter, "Number of tweets:", tweets_counter)
-        
+    #print("reached limit. Did not get all tweets")
+    tweets.to_csv(export_file)
+
     while "next_token" in json_response["meta"].keys():
-        sleep(5)
+        sleep(3)
         query_params["next_token"] = json_response["meta"]["next_token"]
         # in case not all are processed 
         try:
@@ -114,51 +104,90 @@ def get_tweets_by_user(username, export_file):
             tweets_all = pd.concat([tweets_all, tweets], axis=0)
             counter += 1
             tweets_counter += json_response["meta"]["result_count"]
-            print(username, counter, "Number of tweets:", tweets_counter)
+            print(username, " Request:", counter, " N tweets:", tweets_counter)
         except:
             print("reached limit. Did not get all tweets")
+            now = datetime.now()
+            current_time = now.strftime("%H:%M:%S")
+            print("Current Time =", current_time)
+            
         tweets_all.to_csv(export_file)
         
     print("done")
     
+    return
+    
 if __name__ == "__main__":
     
-    usernames = {"Public agencies": ["US_FDA","CDCgov","CDCDirector",
-                                     "Departmentofh14","CDCFund",
-                                     "nycHealthy","PHIdotorg","NIH"],
-                 "NGO":["ResolveTSL","theNAMedicine","WorldHyperLeag"],
-                 "Research and evaluation organization": ["HarvardChanSPH",
-                       "HopkinsMedicine","uwsph","HarvardHealth",
-                       "JohnsHopkinsSPH", "YaleSPH","ColumbiaMSPH"],
+    usernames = {"Public agencies": ["US_FDA","_DCHealth","CDCgov","CDCDirector",
+                                     "CDCFound", "CDC_eHealth",
+                                     "Departmentofh14","MillionHeartsUS",
+                                     "nycHealthy","PHIdotorg","NIH",
+                                     "USDA", "USDANutrition", "TeamNutrition",
+                                     "NationalCACFP", "SNAP_Ed", "BeAFoodHero", "NatWICAssoc"],#
+                 "NGO":["ResolveTSL"],
+                 "Research and evaluation organization": [
+                      "HarvardChanSPH","HarvardHealth","Harvardmed",
+                       "HopkinsMedicine","JohnsHopkinsSPH", 
+                       "uwsph","WUSTLmed", 
+                       "YaleSPH","YaleMed",
+                       "ColumbiaMSPH", "ColumbiaMed"],
                  "Experts": ["chanders4","KBibbinsDomingo","braun_lynne","jesse8850",
                              "eantman","SimonCapewell99","kcferdmd","GardnerPhD","MichelJoffres",
                              "dmljmd","Dmozaffarian","BruceNeal1","DrSaccoNeuro"],
-                "Professional and advocacy assotiations": ["MonellSc","ASPCardio",
-                            "actiononsalt","CardioNerds","American_Heart","AHAScience"],
-                "Partnership": ["MillionHeartsUS","WASHSALT"],
+                "Professional and advocacy assotiations": [
+                    "MonellSc","ASPCardio","CardioNerds",
+                    "American_Heart","AHAScience", "theNAMedicine"],
+                "International influencers": [
+                    "WorldHyperLeag", "worldheartfed", "actiononsalt", "WASHSALT", "WHO"],
                 "Individual influencers": ["DietHeartNews","HealthyHeart4u",
                                            "healthstepsonly","DailyHealthTips",
-                                           "FoodInsight","DailyHealthTips"]
-        }
+                                           "FoodInsight","DailyHealthTips",
+                                           "DrTomFrieden"],
+                "Politicians": ["HillaryClinton", "MichelleObama", "letsmove",
+                                "JoeBiden", "BarackObama"],
+                
+    }
+   
+    usernames_list = []
+    total_usernames = 0
+    for category in usernames.keys():
+        total_usernames += len(usernames[category])
+        usernames_list.extend(usernames[category])
+    print("total accounts:", total_usernames)
 
-    # CDCgov 33072
-    # PHIdotorg 23023 
-    # HarvardChanSPH 31958
-    # actiononsalt 10189
-    # American_Heart 40722
-    # "CDCFund", 
-    #"ColumbiaMSPH",18780
-    #DietHeartNews
-    # FoodInsight
-
-    usernames = ["nycHealthy","DrTomFrieden"]
-    # nycHealthy
-    # DrTomFrieden
+    folder = "/Users/lingchm/Documents/Github/us_sodium_policies/data/twitter/original/"
     
-    folder = "/Users/lingchm/Documents/Github/us_sodium_policies/data/twitter/"
-    for username in usernames:
+    #usernames_list = ["HillaryClinton", "MichelleObama", "letsmove","JoeBiden", "BarackObama"]
+    #usernames_list = ["BarackObama"]
+    
+    for username in usernames_list:
+
+            
+        query_params = {'query': 'from:'+username,
+                'start_time': '2006-07-01T00:00:00Z',
+                'end_time': '2022-06-01T00:00:00Z',
+                'max_results': "500",
+                'sort_order':'recency',
+                'expansions':'author_id,geo.place_id', #'in_reply_to_user_id,entities.mentions.username,referenced_tweets.id.author_id
+                'place.fields':'contained_within,country,country_code,full_name,geo,id,name,place_type',
+                'tweet.fields':'author_id,created_at,entities,geo,id,in_reply_to_user_id,lang,public_metrics,referenced_tweets,text',
+                'user.fields':'created_at,description,id,name,username',
+                'next_token': None
+                }
+        
+        if username in ["HillaryClinton", "MichelleObama", "letsmove","JoeBiden", "BarackObama"]:
+            #query_params['query'] = '(sodium OR salt OR food OR health) from:'+username
+            query_params['query'] = 'health from:'+username
+            continue
+            #query_params['query'] = 'rights from:'+username
+        
         export_file = folder + 'user_'+username+'.csv'
-        get_tweets_by_user(username, export_file)
-        sleep(15*60)
+        if os.path.isfile(export_file):
+            print("Skipping for", username)
+            continue
+        else:
+            get_tweets_by_user(username, query_params, export_file)
+            #sleep(10)
         
         
